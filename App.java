56ce51cfedd76dcb7739fbb07b250a3217a32277
JesusRiestra <jriestraricote@gmail.com>
2022-12-20 12:30:50 +0100

    App.java y README.md modificados por Jesús Riestra

--- App.java
+++ App.java
@@ -1,9 +1,12 @@
 public class App {
   
   public static void main(String[] args) {
+    System.out.println("Jesús Riestra Ricote");
     System.out.println("Contenido 2a");
     System.out.println("Contenido 3");
     System.out.println("Contenido 2b");
+    System.out.println("20/12/2022");
+    System.out.println("Villanueva del Carnero, León");
   }
   
 }
